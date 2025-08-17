f434ac79f892c7a798c71fdae61908139a897170
shitalparmar443 <shitalparmar443@gmail.com>
2025-08-17 15:40:41 +0530

    Add files via upload

---
+++ languages/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Prevent direct access to this file.
+ *
+ * @package Issues Exporter for GitLab
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Silence is golden.
